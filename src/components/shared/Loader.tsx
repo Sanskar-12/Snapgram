f522d2cefba6874538439970749b1ff0cc0d6388
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-24 10:31:56 +0530

    Form completed setting up appwrite

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,14 @@
+const Loader = () => {
+  return (
+    <div className="flex-center w-full">
+      <img
+        src={"/assets/icons/loader.svg"}
+        alt="Loader Icon"
+        width={24}
+        height={24}
+      />
+    </div>
+  );
+};
+
+export default Loader;
