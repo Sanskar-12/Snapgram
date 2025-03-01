9c12130efdca203ebe6af7c8122bc94e8eddceae
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-01 10:36:17 +0530

    Topbar UI and Sign out functionality done

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -74,9 +74,9 @@ export const AuthProvider=({children}:{ // 3. Create Provider
   useEffect(()=>{
     const cookieFallback = localStorage.getItem("cookieFallback");
     if (
-      cookieFallback === "[]" 
-      // cookieFallback === null ||
-      // cookieFallback === undefined
+      cookieFallback === "[]" ||
+      cookieFallback === null ||
+      cookieFallback === undefined
     ) {
       navigate("/sign-in");
     }
